778110975bfe7de0c7aa09f50b633b83e3051831
s5s5cn <s5s5cn@e0db21db-5650-0410-bdc3-bdf3cf9a6094>
2011-05-14 16:07:45 +0000

    InstaQQt 代码准备

---
+++ InstaQQt/processorder.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Bob's Auto Parts - Order Results</title>
+</head>
+<h1>Bob's Auto Parts</h1>
+<h2>Order Results</h2>
+<?php
+	echo '<p>Order processed at';
+	echo date('H:i,jS F');
+	echo '.</p>';
+?>
+<body>
+</body>
+</html>
