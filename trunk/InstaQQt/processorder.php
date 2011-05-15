0e2a341aba5cf2c924233ba722db4469c3de5b47
s5s5cn <s5s5cn@e0db21db-5650-0410-bdc3-bdf3cf9a6094>
2011-05-15 04:59:27 +0000

    InstaQQt 代码准备

--- trunk/InstaQQt/processorder.php
+++ trunk/InstaQQt/processorder.php
@@ -7,9 +7,17 @@
 <h1>Bob's Auto Parts</h1>
 <h2>Order Results</h2>
 <?php
+	// create short variable names
+	$tireqty = $_POST['tireqty'];
+	$oilqty = $_POST['oilqty'];
+	$sparkqty = $_POST['sparkqty'];
 	echo '<p>Order processed at';
 	echo date('H:i,jS F');
 	echo '.</p>';
+	echo '<p>Your order is as follows:</p>';
+	echo $tireqty.' tires<br>';
+	echo $oilqty.' bottles of oil<br>';
+	echo $sparkqty.' spark plugs<br>';
 ?>
 <body>
 </body>
