00e4ad3df8d45ead647ecfeca05234aa70262e52
s5s5cn@gmail.com <s5s5cn@gmail.com@e0db21db-5650-0410-bdc3-bdf3cf9a6094>
2011-07-28 14:15:09 +0000

    python ex5

--- trunk/Python/ex5.py
+++ trunk/Python/ex5.py
@@ -2,21 +2,23 @@
 # -*- coding: utf-8 -*-
 # Filename : ex5.py
 
-my_name = 'Zed A. shaw'
-my_age = 35 # not a lie
-my_height = 74 # inches
-my_weight = 180 # lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Zed A. shaw'
+age = 35 # not a lie
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+height2 = height * 2.54
+weight2 = weight * 0.45359237
 
-print "Let's talk about %s." % my_name
-print "He's %d inches tall." % my_height
-print "He's %d pounds heavy." % my_weight
+print "Let's talk about %s." % name
+print "He's %d inches (%f CM) tall." % (height, height2)
+print "He's %d pounds (%f Kg.) heavy." % (weight, weight2)
 print "Actually that's not too heavy."
-print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
-print "His teeth are usually %s depending on the coffee." % my_teeth
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
 
 # this line is tricky, try to get it exactly right
 print "If I add %d, %d, and %d I get %d." % (
-    my_age, my_height, my_weight, my_age + my_height + my_weight)
+    age, height, weight, age + height + weight)
