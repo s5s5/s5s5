10a934c958a7ecee3bdb35772db4823434c8dab2
s5s5cn@gmail.com <s5s5cn@gmail.com@e0db21db-5650-0410-bdc3-bdf3cf9a6094>
2011-08-31 08:35:25 +0000

    python exercise

--- trunk/Python/temp.py
+++ trunk/Python/temp.py
@@ -1,2 +1,5 @@
-x = raw_input('x:')
-print "l" +x+ "r"
+for x in range(1, 100/5+1):
+    for y in range(1, 100/3+1):
+        for z in range(3, 100*3+1, 3):
+            if 5*x+3*y+z/3==100 and x+y+z==100:
+                print x, y, z
