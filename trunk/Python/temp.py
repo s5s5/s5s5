92d79fa7299a1d1c10c0dd9f6ef707d03939c8a6
s5s5cn <s5s5cn@e0db21db-5650-0410-bdc3-bdf3cf9a6094>
2011-09-05 08:34:27 +0000

    python exercise

--- trunk/Python/temp.py
+++ trunk/Python/temp.py
@@ -1,5 +1,8 @@
-for x in range(1, 100/5+1):
-    for y in range(1, 100/3+1):
-        for z in range(3, 100*3+1, 3):
-            if 5*x+3*y+z/3==100 and x+y+z==100:
-                print x, y, z
+x = 100
+y = 50
+z = 150
+if x > y:
+    print "x > y"
+
+elif z > y:
+    print "z > y"
