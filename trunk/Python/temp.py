c59c321bd7c6aef1ac326810c89901d78ce2988b
s5s5cn@gmail.com <s5s5cn@gmail.com@e0db21db-5650-0410-bdc3-bdf3cf9a6094>
2011-09-11 06:54:58 +0000

    no

--- trunk/Python/temp.py
+++ trunk/Python/temp.py
@@ -1,8 +1,32 @@
-x = 100
-y = 50
-z = 150
-if x > y:
-    print "x > y"
+# -*- coding:utf-8 -*-
 
-elif z > y:
-    print "z > y"
+def xyz1():
+    '''
+        解不定方程
+        x+y+z=100
+        5*x+3*y+z/3=100
+        x,y,z都是整数并且大于0
+    '''
+    #方程可以化简成7*x+4*y=100
+    for x in range(1, 100 / 5):
+        y1 = (100 - 7 * x)
+        y = int(y1 / 4)
+        if y * 4 == y1 and y >= 0:
+            print [x, y, 100 - x - y]
+    return
+
+
+def xyz2():
+    for x in range(1, 100 / 5):
+        for y in range(1, 100 / 3):
+            for z in range(1, 100 * 3):
+                z1 = z / 3
+                z = int(z1 * 3)
+
+                if 5 * x + 3 * y + z / 3 == 100 and x + y + z == 100:
+
+                    print x, y, z
+    return
+
+print xyz1()
+print xyz2()
